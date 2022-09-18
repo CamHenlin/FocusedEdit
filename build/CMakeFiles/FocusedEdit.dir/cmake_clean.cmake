431f1464acff201dd5cd46b1a00ec2fc48ee4396
camh <cam.henlin@gmail.com>
2022-09-17 23:43:45 -0700

    REALLY adding the build files[

---
+++ build/CMakeFiles/FocusedEdit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FocusedEdit.dir/SerialHelper.c.obj"
+  "CMakeFiles/FocusedEdit.dir/TESample.c.obj"
+  "CMakeFiles/FocusedEdit.dir/coprocessorjs.c.obj"
+  "FocusedEdit.code.bin"
+  "FocusedEdit.code.bin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FocusedEdit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/FocusedEdit_APPL.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "%FocusedEdit.ad"
+  "CMakeFiles/FocusedEdit_APPL"
+  "FocusedEdit.APPL"
+  "FocusedEdit.ad"
+  "FocusedEdit.bin"
+  "FocusedEdit.dsk"
+  "TESample.r.rsrc.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/FocusedEdit_APPL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
