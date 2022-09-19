64005f90c4fb3d622687986c9c18857bbee0f6f3
camh <cam.henlin@gmail.com>
2022-09-18 23:54:32 -0700

    remove debugging

--- TESample.c
+++ TESample.c
@@ -270,7 +270,7 @@ Boolean TrapAvailable(short tNumber, TrapType tType)
 int main()
 {
 
-	// MaxApplZone();
+	// MaxApplZone(); // this seems to entirely break the program within the coprocessor code
 	Initialize();					/* initialize the program */
 	UnloadSeg((Ptr) Initialize);	/* note that Initialize must not be in Main! */
 
